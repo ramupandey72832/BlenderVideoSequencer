907d306fcb211363d01376b9ea6b6cb73123e842
ramupandey72832 <ramupandey72832@gmail.com>
2026-03-17 13:14:09 +0530

    Enhance subtitle generation with color settings
    Added functionality to set subtitle colors and styles.

--- TypeWritterFor Caption/mainFile_useinblendFile.py
+++ TypeWritterFor Caption/mainFile_useinblendFile.py
@@ -14,7 +14,9 @@ import text_tool
 import text_effects
 importlib.reload(text_effects)
 importlib.reload(text_tool) # Forces Blender to see your latest changes
-
+bpy.context.scene["subtitle_pause_time"] = 0.8
+bpy.context.scene["sub_text_color"] = (0.0, 0.0, 0.0, 1.0) # White
+bpy.context.scene["sub_bg_color"]   = (1.0, 1.0, 1.0, 0.6) # Black 60% Alpha
 # 3. SETTINGS
 CSV_PATH = os.path.join(script_dir, "subtitle.csv")
 SOUND_PATH = os.path.join(script_dir,"typeWritter.mp3")
@@ -28,6 +30,13 @@ text_tool.create_subtitles(CSV_PATH, CHANNEL)
 sequencer = bpy.context.scene.sequence_editor.strips
 for s in sequencer:
     if s.channel == 10 and s.type == 'TEXT':
+        # Apply style using the scene colors
+        text_effects.set_style(
+            s, 
+            b_color=bpy.context.scene["sub_bg_color"], 
+            t_color=bpy.context.scene["sub_text_color"],
+            size=65
+        )
         text_effects.apply_typewriter(s)
         text_effects.add_typewriter_audio(s, SOUND_PATH)
 
